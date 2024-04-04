62a219784f5dbd8f38cb332c3a608f0e63beeea2
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-04-04 11:40:09 +0700

    untuk melakukan pencarian linear (secara berurutan) terhadap suatu nilai di dalam array

--- tugasSearch/tugasSearch.cpp
+++ tugasSearch/tugasSearch.cpp
@@ -19,6 +19,8 @@ void input() {
 	cout << " Enter array elements \n";
 	cout << "----------------------\n";
 	for (i = 0; i < n; i++) {
-		cout << "<" << (i + 1); 
+		cout << "<" << (i + 1);
 		cin >> arr[i];
 	}
+}
+void LinearSearch() {
