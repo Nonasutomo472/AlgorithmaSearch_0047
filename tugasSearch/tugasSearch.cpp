186f206902c30ec99a3867bdd4a2d6ee764ad865
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-04-04 11:22:34 +0700

    Add project files.

---
+++ tugasSearch/tugasSearch.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; //Array to be searched
+int n; //Number of elements the array
+int i; //index of array element 
+
+void input() {
+    while (true) {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 maximum 20 element.\n\n";
+    }
+
+    //Accept array element
+    cout << "\n ----------------------\n";
+    cout << " Enter array elements \n";
+    cout << "----------------------\n";
+    for (i = 0; i < n; i++) {
+        cout << "<" << (i + 1); //Tambahkan titik koma di sini
+        cin >> arr[i];
+    }
+}
+
+void LinearSearch() {
+    char ch;
+    int ctr; // number of comparisons
+    int item;
+
+    do {
+        //Accept the number to be searched
+        cout << "\nEnter the element you want to search:"; // step 1
+        cin >> item;
+
+        ctr = 0;
+        i = 0; //step 2
+        while (i < n) // step 3
+        {
+            ctr++;
+            if (arr[i] == item)
+            {
+                cout << "\n" << item << " found at position " << (i + 1) << endl; //step 6
+                break;
+            }
+            i++; // step 4
+        }
+        if (i == n) // step 5
+            cout << "\n" << item << " not found in the array\n";
+        cout << "\nNumber of comparisons: " << ctr << endl;
+
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
+    } while ((ch == 'y') || (ch == 'Y'));
+}
+
+void display() {
+    cout << " Array elements are: ";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    input();
+    LinearSearch();
+    display();
+    return 0; //Tambahkan ini untuk menyelesaikan fungsi main
+}
